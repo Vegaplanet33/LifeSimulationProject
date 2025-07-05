9aed98ee8dc775c8688fcfd86f4b7e2d207958d6
Dohi Kim <vegaplanet33@gmail.com>
2025-07-05 11:50:29 +0900

    파일 수정 및 새 파일 추가

--- life.py
+++ life.py
@@ -1,18 +1,1 @@
-# 생명체 초기 설정
-name="바오"         # 생명체 이름
-energy = 8         # 에너지 수치 (0~10)
-mood = "happy"     # 기분상태
-
-print (">>> act() 함수 실행 시작")
-def act(name, mood, energy):
-    if mood == "happy" and energy >5:
-        print(f"{name}는 산책 나갔어요!🚶‍♂️")
-    elif mood == "tired" or energy < 3:
-        print(f"{name}는 조용히 누워서 자고 있어요 😴")
-    elif mood == "angry":
-        print(f"{name}는 화가 나서 으르릉~~! 소리를 냈어요. 😡💥")
-    else:
-        print(f"{name}는 멍하니 창 밖을 바라보고 있어요. 😶")
-
-    print (">>> act() 함수 호출 전입니다.")
-    act(name, mood, energy)
+from modules.actions import act
---
+++ modules/actions.py
@@ -0,0 +1,10 @@
+def act(name, mood, energy):
+    print(">> act() 함수 실행 시작")
+    if mood == "happy" and energy > 5:
+        print(f"{name}는 산책 나갔어요!🚶‍♂️")
+    elif mood == "tired" or energy < 3:
+        print(f"{name}는 조용히 누워서 자고 있어요 😴")
+    elif mood == "angry":
+        print(f"{name}는 화가 나서 으르릉~~! 소리를 냈어요. 😡💥")
+    else:
+        print(f"{name}는 멍하니 창 밖을 바라보고 있어요. 😶")
